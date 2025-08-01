2239783d6fdfc0349991739437ec6330fe47adcc
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-01 14:15:45 +0700

    feat: install shadecn/ui

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const HeroSection = () => {
+  return (
+    <div>
+      HeroSection
+    </div>
+
+  )
+}
+
+export default HeroSection
