f6ebf87650938c6572ba910cca9674b9ceff9086
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-07 13:45:10 +0700

    feat: add Footer Component and its styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-    <div>
+    <div className="flex flex-col">
       <Navbar/>
       <HeroSection/>
+      <Footer />
     </div>
     </>
   )
