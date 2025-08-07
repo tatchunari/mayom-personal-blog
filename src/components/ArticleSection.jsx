b9b9c5edb49205b177a220e14df2cb131438ef29
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-08-07 15:17:47 +0700

    feat: add BlogCard(s) using props

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,5 +1,7 @@
 import ArticleNavbarMobile from "./ArticleNavbarMobile";
 import ArticleNavbarDesktop from "./ArticleNavbarDesktop";
+import BlogCard from "./BlogCard";
+import { blogPosts } from "@/data/blogPost";
 
 const ArticleSection = () => {
   return (
@@ -14,6 +16,24 @@ const ArticleSection = () => {
         <div className="hidden md:block">
           <ArticleNavbarDesktop />
         </div>
+      </div>
+
+      {/* Blog/Article Cards Section */}
+      <div className="max-w-5xl mx-auto px-4 mt-6 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {
+         blogPosts.map((post) => 
+          <BlogCard 
+            key={post.id}
+            image={post.image}
+            category={post.category}
+            title={post.title}
+            description={post.description}
+            author={post.author}
+            date={post.date} 
+          />
+
+        ) 
+        }
       </div>
     </section>
   );
